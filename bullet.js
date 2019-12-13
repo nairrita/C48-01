d89f79dc1a1d3949ca76dc6fe125cce21fd7901f
Nairrita <nairrita@Nairritas-MacBook-Air.local>
2019-12-13 13:08:06 +0530

    added score

---
+++ bullet.js
@@ -0,0 +1,31 @@
+class Bullet{
+    constructor(x,y){
+        this.x=x;
+        this.y=y;
+        this.height=20;
+        this.width=10
+       this.image = loadImage("images/daBullt.jpg");
+
+    };
+      
+   hits(player1){
+    var d = dist(this.x,this.y,player1.x,player1.y);
+    if(d<this.height+player1.y){
+        return true;
+    }
+    else{
+        return false;
+    }
+    }
+    
+    fire(bullet_pos){
+        this.x = this.x+bullet_pos*5;
+    }
+    display(){
+        fill(255,255,255);
+        imageMode(CENTER);
+        image(this.image,this.x,this.y,this.width,this.height);
+    }
+
+   
+}
